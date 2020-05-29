29b239b8d6b53e87cc87102a0c75cb3163cc0384
xuexiao <xuexiao@belink.com>
2020-05-29 17:52:09 +0800

    配合app10测试的后台服务

---
+++ src/app11/server.js
@@ -0,0 +1,25 @@
+/**
+ * 充当app10的后端服务
+ * @type {Application}
+ */
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx) => {
+    ctx.response.type = 'application/json';
+
+    const responseBody = {
+        result: {
+            code: 0,
+            description: 'success'
+        }, data: {
+            username: 'zhangsan',
+            address: 'taiyuan',
+            age: 20
+        }
+    };
+
+    ctx.body = JSON.stringify(responseBody);
+});
+
+app.listen(4000);
